58ad945da053916063065cb69ef96e5467411599
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-01-21 21:06:30 -0700

    worked on the base for a 8PT auton
    made simple modifications to the bottom red code

---
+++ 8PT/include/robot-config.h
@@ -0,0 +1,20 @@
+using namespace vex;
+
+extern brain Brain;
+extern motor leftFWD;
+extern motor leftBack;
+extern motor rightFWD;
+extern motor rightBack;
+extern motor CubeRamp;
+extern motor intakeRight;
+extern motor intakeLeft;
+extern controller Controller1;
+extern controller Controller2;
+extern bumper rampBumper;
+extern bumper rampBumperForward;
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void);
