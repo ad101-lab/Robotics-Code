a9aae2d6469697c472608c9075a1a75cc7f855b8
VEX-EDR-SKYNET <59324835+VEX-EDR-SKYNET@users.noreply.github.com>
2020-02-12 18:45:02 -0700

    created costom header files.

--- In Progress/newRobot2control-headers/include/robot-config.h
+++ In Progress/newRobot2control-headers/include/robot-config.h
@@ -1,5 +1,18 @@
 #include "vex.h"
-#include<string.h>
+using namespace vex;
+
+extern brain Brain;
+extern motor leftFWD;
+extern motor leftBack;
+extern motor rightFWD;
+extern motor rightBack;
+extern motor CubeRamp;
+extern motor intakeRight;
+extern motor intakeLeft;
+extern controller Controller1;
+extern controller Controller2;
+extern bumper rampBumper;
+extern bumper rampBumperForward;
 using namespace vex;
 
 competition Competition;
@@ -178,85 +191,3 @@ void oneBarTower(std::string tower){
   
 }
 
-void oneBarStop(){
-  while(1){
-    if(oneBar.rotation(rev) > 5 or oneBar.rotation(rev) < 0){
-      oneBar.stop();
-    }
-  }
-}
-
-
-void pre_auton(void) {
-  motorHold(true);
-}
-
-void autonomous(void) {
-  intake(100);
-  oneBarUp(45, 100, true);
-  oneBarUp(-45, 100, true);
-  //moveForward(, int speed)
-}
-
-void usercontrol(void) {//User Control
-  while (1) {
-    rightFWD.spin(forward, (Controller1.Axis2.position()/ turnValue)/baseRPM , vex::velocityUnits::pct);//Tank Drive controls
-    leftFWD.spin(forward, (Controller1.Axis3.position()/ turnValue)/baseRPM , vex::velocityUnits::pct);
-    rightBack.spin(forward, (Controller1.Axis2.position()/ turnValue)/baseRPM , vex::velocityUnits::pct);
-    leftBack.spin(forward, (Controller1.Axis3.position()/ turnValue)/baseRPM , vex::velocityUnits::pct);
-    if (Controller2.ButtonL1.pressing() and !(cubeRamp.rotation(rev)>3.7)){//if button is pressing it will
-      cubeRampValue = 85;//sets cube ramp to 85 RPM
-    } else if (Controller2.ButtonL2.pressing() and !(cubeRamp.rotation(rev)<0)) {//if button is pressing it will
-      cubeRampValue = -100;//sets cube ramp to -100 RPM
-    } else {//if no others are true
-      cubeRampValue = 0;//Stops cube ramp
-    }
-    cubeRamp.spin(forward, cubeRampValue , vex::velocityUnits::rpm);//applies the changes
-    if (Controller1.ButtonR1.pressing()){//if button is pressing it will
-      intakeValue = 100;//sets cube ramp to 100 RPM
-    } else if (Controller1.ButtonR2.pressing()) {//if button is pressing it will
-      intakeValue = -200;//sets cube ramp to -200 RPM
-    } else if (Controller1.ButtonA.pressing()){//if button is pressing it will
-      intakeValue = -50;//sets cube ramp to -50 RPM
-    }else if(Controller1.ButtonB.pressing()){//if button is pressing it will
-      intakeValue = 45;//sets cube ramp to 45 RPM
-    } else if(Controller2.ButtonX.pressing()){//if button is pressing it will
-      stack();//Stacks
-    } else {//If no other conditions are true
-      intakeValue = 0;//sets cube ramp to -100 RPM
-    }
-    if(Controller2.ButtonUp.pressing() and !(oneBar.rotation(rev)>1.7)){
-      oneBarValue = 200;
-    }else if (Controller2.ButtonDown.pressing()and !(oneBar.rotation(rev)<0)) {
-      oneBarValue = -200;
-    } else {
-      oneBarValue = 0;
-    }
-    if (Controller2.ButtonR1.pressing()){
-      baseRPM = 6;
-    } else if (Controller2.ButtonR2.pressing()){
-      baseRPM = 2;
-    }else {
-      baseRPM = 1;
-    }
-    if (((Controller1.Axis3.value() > 60) and (Controller1.Axis2.value() < -60)) or ((Controller1.Axis3.value() < -60) and (Controller1.Axis2.value() > 60))){
-      turnValue = 3;
-    } else{
-      turnValue = 1;
-    }
-    oneBar.spin(forward, oneBarValue, pct);
-    intakeLeft.spin(forward, intakeValue , vex::velocityUnits::rpm);//applies the changes
-    intakeRight.spin(forward, intakeValue , vex::velocityUnits::rpm);
-    wait(20, msec); // Sleep the task for a short amount of time to
-    Brain.Screen.print(oneBar.rotation(rev));
-  }
-}
-
-int main() {
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
-  pre_auton();
-  while (true) {
-    wait(100, msec);
-  }
-}
