90b9cadeca707b8bcef6b7dfbaee68e387ecff71
VEX-EDR-SKYNET <59324835+VEX-EDR-SKYNET@users.noreply.github.com>
2020-02-29 17:21:47 -0700

    worked on PID

--- In Progress/newRobot2control-headers-PID/include/robot-config.h
+++ In Progress/newRobot2control-headers-PID/include/robot-config.h
@@ -61,6 +61,129 @@ bool autonColor;
 bool userControlEnabled = false;
 std::string tower;
 
+//PID timer
+
+double kP = 0.0;
+double kI = 0.0;
+double kD = 0.0;
+double turnkP = 0.0;
+double turnkI = 0.0;
+double turnkD = 0.0;
+int maxTurnIntegral = 300; // These cap the integrals
+int maxIntegral = 300;
+int integralBound = 3; //If error is outside the bounds, then apply the integral. This is a buffer with +-integralBound degrees
+
+//Autonomous Settings
+int desiredValue = 200;
+int desiredTurnValue = 0;
+
+int error; //SensorValue - DesiredValue : Position
+int prevError = 0; //Position 20 miliseconds ago
+int derivative; // error - prevError : Speed
+int totalError = 0; //totalError = totalError + error
+
+int turnError; //SensorValue - DesiredValue : Position
+int turnPrevError = 0; //Position 20 miliseconds ago
+int turnDerivative; // error - prevError : Speed
+int turnTotalError = 0; //totalError = totalError + error
+
+bool resetDriveSensors = false;
+
+bool enableDrivePID = true;
+
+double signnum_c(double x) {
+  if (x > 0.0) return 1.0;
+  if (x < 0.0) return -1.0;
+  return x;
+}
+
+int drivePID(){
+  
+  while(enableDrivePID){
+
+    if (resetDriveSensors) {
+      resetDriveSensors = false;
+      leftFWD.setPosition(0,degrees);
+      rightFWD.setPosition(0,degrees);
+      leftBack.setPosition(0,degrees);
+      rightBack.setPosition(0,degrees);
+    }
+
+
+    //Get the position of both motors
+    double leftFWDPosition = leftFWD.position(degrees);
+    double leftBackPosition = leftBack.position(degrees);
+    double rightFWDPosition = rightFWD.position(degrees);
+    double rightBackPosition = rightBack.position(degrees);
+
+    double averagePosition = (leftFWDPosition + leftBackPosition + rightFWDPosition + rightBackPosition)/4;
+
+    //Potential
+    error = averagePosition - desiredValue;
+
+    //Derivative
+    derivative = error - prevError;
+
+    //Integral
+    if(abs(error) < integralBound){
+    totalError+=error; 
+    }  else {
+    totalError = 0; 
+    }
+    //totalError += error;
+
+    //This would cap the integral
+    totalError = abs(totalError) > maxIntegral ? signnum_c(totalError) * maxIntegral : totalError;
+
+    double lateralMotorPower = error * kP + derivative * kD + totalError * kI;
+    /////////////////////////////////////////////////////////////////////
+
+
+    ///////////////////////////////////////////
+    //Turning movement PID
+    /////////////////////////////////////////////////////////////////////
+    //Get average of the two motors
+    double turnDifference = turnInertial.rotation();
+    //Potential
+    turnError = turnDifference - desiredTurnValue;
+
+    //Derivative
+    turnDerivative = turnError - turnPrevError;
+
+    //Integral
+    if(abs(error) < integralBound){
+    turnTotalError+=turnError; 
+    }  else {
+    turnTotalError = 0; 
+    }
+    //turnTotalError += turnError;
+
+    //This would cap the integral
+    turnTotalError = abs(turnTotalError) > maxIntegral ? signnum_c(turnTotalError) * maxIntegral : turnTotalError;
+
+    double turnMotorPower = turnError * turnkP + turnDerivative * turnkD + turnTotalError * turnkI;
+
+    rightFWD.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
+    rightBack.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
+    leftFWD.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
+    leftBack.spin(forward, lateralMotorPower - turnMotorPower, voltageUnits::volt);
+
+
+    /*rightFWD
+    rightBack
+    leftFWD
+    leftBack*/
+
+    prevError = error;
+    turnPrevError = turnError;
+    vex::task::sleep(20);
+
+  }
+
+  return 1;
+}
+
+//End PID
 void moveForward(double cm, double speed, bool stopping){
   leftFWD.spinFor(forward, cm, rev, speed, velocityUnits::pct, false);
   rightFWD.spinFor(forward, cm, rev, speed, velocityUnits::pct, false);
